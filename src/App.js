57ed6f751cb680b8f1157b707891d7e73686f774
vimalestrrado <vimalvyshagam@gmail.com>
2023-02-03 00:00:17 +0530

    unknow changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React, { Suspense } from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { MainLayout } from "./Components/Layout";
-import OnePageWebSite from "./Pages/OnePageWebSite/OnePageWebSite";
+import React, { Suspense } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { MainLayout } from './Components/Layout';
+import OnePageWebSite from './Pages/OnePageWebSite/OnePageWebSite';
 
 function App() {
   return (
